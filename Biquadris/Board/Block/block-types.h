5324f54bc8223ee2e809ea16dd436133be6ab09a
fehervaryy <martindmhe@gmail.com>
2024-11-23 16:29:04 -0500

    add alias types for block shape

--- Biquadris/Board/Block/block-types.h
+++ Biquadris/Board/Block/block-types.h
@@ -5,69 +5,70 @@
 
 // Rotation is protected, could make public to use this alias
 // using ShapeMap = std::unordered_map<Block::Rotation, std::vector<std::pair<int, int>>>;
+using Shape = std::vector<std::pair<int, int>>;
 
 class IBlock : public Block {
     private:
-        std::unordered_map<Rotation, std::vector<std::pair<int, int>>> baseShapeRotations;
+        std::unordered_map<Rotation, Shape> baseShapeRotations;
     public:
         IBlock(BoardProxy& board, int level);
-        std::vector<std::pair<int, int>> getBaseShape(Rotation rotation) override;
+        Shape getBaseShape(Rotation rotation) override;
 };
 
 class JBlock : public Block {
     private:
-        std::unordered_map<Rotation, std::vector<std::pair<int, int>>> baseShapeRotations;
+        std::unordered_map<Rotation, Shape> baseShapeRotations;
     public:
         JBlock(BoardProxy& board, int level);
-        std::vector<std::pair<int, int>> getBaseShape(Rotation rotation) override;
+        Shape getBaseShape(Rotation rotation) override;
 };
 
 class LBlock : public Block {
     private:
-        std::unordered_map<Rotation, std::vector<std::pair<int, int>>> baseShapeRotations;
+        std::unordered_map<Rotation, Shape> baseShapeRotations;
     public:
         LBlock(BoardProxy& board, int level);
-        std::vector<std::pair<int, int>> getBaseShape(Rotation rotation) override;
+        Shape getBaseShape(Rotation rotation) override;
 };
 
 class OBlock : public Block {
     private:
-        std::unordered_map<Rotation, std::vector<std::pair<int, int>>> baseShapeRotations;
+        std::unordered_map<Rotation, Shape> baseShapeRotations;
     public:
         OBlock(BoardProxy& board, int level);
-        std::vector<std::pair<int, int>> getBaseShape(Rotation rotation) override;
+        Shape getBaseShape(Rotation rotation) override;
 };
 
 class SBlock : public Block {
     private:
-        std::unordered_map<Rotation, std::vector<std::pair<int, int>>> baseShapeRotations;
+        std::unordered_map<Rotation, Shape> baseShapeRotations;
     public:
         SBlock(BoardProxy& board, int level);
-        std::vector<std::pair<int, int>> getBaseShape(Rotation rotation) override;
+        Shape getBaseShape(Rotation rotation) override;
 };
 
 class ZBlock : public Block {
     private:
-        std::unordered_map<Rotation, std::vector<std::pair<int, int>>> baseShapeRotations;
+        std::unordered_map<Rotation, Shape> baseShapeRotations;
     public:
         ZBlock(BoardProxy& board, int level);
-        std::vector<std::pair<int, int>> getBaseShape(Rotation rotation) override;
+        Shape getBaseShape(Rotation rotation) override;
 };
 
 class TBlock : public Block {
     private:
-        std::unordered_map<Rotation, std::vector<std::pair<int, int>>> baseShapeRotations;
+        std::unordered_map<Rotation, Shape> baseShapeRotations;
     public:
         TBlock(BoardProxy& board, int level);
-        std::vector<std::pair<int, int>> getBaseShape(Rotation rotation) override;
+        Shape getBaseShape(Rotation rotation) override;
 };
 
 class StarBlock : public Block {
     private:
-        std::unordered_map<Rotation, std::vector<std::pair<int, int>>> baseShapeRotations;
+        std::unordered_map<Rotation, Shape> baseShapeRotations;
     public:
         StarBlock(BoardProxy& board, int level);
-        std::vector<std::pair<int, int>> getBaseShape(Rotation rotation) override;
+        Shape getBaseShape(Rotation rotation) override;
         int getClearScore();
 };
 
