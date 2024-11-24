be488bdfa4aa3fecf399ed1a028882a8c1fd05f2
Henry Yang <hhenryyangg@gmail.com>
2024-11-24 00:44:17 -0500

    completed level class; dont know why intellesense is tweaking in level.cc

--- Biquadris/Board/Level/level.cc
+++ Biquadris/Board/Level/level.cc
@@ -1,11 +1,73 @@
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+#include <utility>
+#include <cstdlib>
 #include "level.h"
+#include "../Block/block-types.h"
 
-Level::Level(int numLevel, bool heavy, BoardProxy & boardProxy, bool random, std::string sequenceFileName) :
-  numLevel{numLevel}, heavy{heavy}, boardProxy{boardProxy}, random{random}, sequenceFileName{sequenceFileName} {
+// intellesense tweakin wtf
+
+// Level definitions
+Level::Level(int numLevel, bool heavy, BoardProxy & boardProxy, std::string & blockSequenceFileName) :
+  numLevel{numLevel}, heavy{heavy}, boardProxy{boardProxy}, blockSequenceFileName{std::move(blockSequenceFileName)}, 
+  blockSequenceFileStream{std::ifstream{blockSequenceFileName}} {
+
+  if (!blockSequenceFileStream.is_open()) {
+    throw std::runtime_error("Failed to open \"" + blockSequenceFileName + "\" block sequence file");
+  }
 
   for (int i = 0; i < BACKLOG_SIZE; i++) {
-    this->blockBacklog.push_back(this->generateBlock());
+    this->blockBacklog.push_back(this->generateBlock(this->chooseBlockType()));
   }
+}
+
+std::unique_ptr<Block> Level::generateBlock(char blockType) const {
+  switch (blockType) {
+    case 'I': return std::make_unique<IBlock>(this->boardProxy, this->numLevel);
+    case 'J': return std::make_unique<JBlock>(this->boardProxy, this->numLevel);
+    case 'L': return std::make_unique<LBlock>(this->boardProxy, this->numLevel);
+    case 'O': return std::make_unique<OBlock>(this->boardProxy, this->numLevel);
+    case 'S': return std::make_unique<SBlock>(this->boardProxy, this->numLevel);
+    case 'Z': return std::make_unique<ZBlock>(this->boardProxy, this->numLevel);
+    case 'T': return std::make_unique<TBlock>(this->boardProxy, this->numLevel);
+    case 'i': return std::make_unique<IBlock>(this->boardProxy, this->numLevel);
+    case 'j': return std::make_unique<JBlock>(this->boardProxy, this->numLevel);
+    case 'l': return std::make_unique<LBlock>(this->boardProxy, this->numLevel);
+    case 'o': return std::make_unique<OBlock>(this->boardProxy, this->numLevel);
+    case 's': return std::make_unique<SBlock>(this->boardProxy, this->numLevel);
+    case 'z': return std::make_unique<ZBlock>(this->boardProxy, this->numLevel);
+    case 't': return std::make_unique<TBlock>(this->boardProxy, this->numLevel);
+  }
+}
+
+void Level::setBlockSequenceFile(std::string & blockSequenceFile) {
+  std::ifstream tempFileStream{blockSequenceFile};
+
+  if (!tempFileStream.is_open()) {
+    throw std::runtime_error("Failed to open \"" + blockSequenceFile + "\" block sequence file; using \"" + this->blockSequenceFileName + "\" instead");
+  }
+
+  this->blockSequenceFileName = blockSequenceFile;
+  this->blockSequenceFileStream = std::move(tempFileStream);
+}
+
+char Level::chooseBlockType() const {
+  char blockType;
+
+  this->blockSequenceFileStream >> std::ws;
+  this->blockSequenceFileStream >> blockType;
+
+  if (this->blockSequenceFileStream.fail()) {
+    this->blockSequenceFileStream.close();
+    this->blockSequenceFileStream.open(this->blockSequenceFileName);
+    this->blockSequenceFileStream >> std::ws;
+    this->blockSequenceFileStream >> blockType;
+    if (this->blockSequenceFileStream.fail()) {
+      throw std::ios_base::failure("Provided block sequence file \"" + this->blockSequenceFileName + "\" is invalid.");
+    }
+  }
+  return blockType;
 }
 
 std::unique_ptr<Block> Level::cycleBlock() {
@@ -15,4 +77,48 @@ std::unique_ptr<Block> Level::cycleBlock() {
   this->blockBacklog.push_back(this->generateBlock());
 
   return std::move(poppedBlock);
+}
+
+bool Level::checkRuleCondition() const { return false; }
+
+void Level::executeRuleAction() {}
+
+
+const char BLOCK_TYPE_ORDER[] = {'I', 'J', 'L', 'O', 'S', 'Z', 'T'};
+
+// RandomizedLevel definitions
+RandomizedLevel::RandomizedLevel(int numLevel, bool heavy, BoardProxy & boardProxy, std::string & blockSequenceFileName, int denom, std::vector<int> & distributionNumerators) :
+  Level{numLevel, heavy, boardProxy, blockSequenceFileName}, denom{denom}, 
+  blockCumulativeDistributionMap{std::move(convertNumeratorsToCumulativeDistributionMap(distributionNumerators))} {}
+
+std::map<int, char> RandomizedLevel::convertNumeratorsToCumulativeDistributionMap(std::vector<int> & distributionNumerators) {
+  std::map<int, char> outputBlockCumulativeDistributionMap;
+  int cumulativeKeyAdjustment = 0;
+  
+  for (size_t i = 0; i < distributionNumerators.size(); i++) {
+    for (int j = 0; j < distributionNumerators[i]; j++) {
+      outputBlockCumulativeDistributionMap[cumulativeKeyAdjustment + j] = BLOCK_TYPE_ORDER[i];
+    }
+    cumulativeKeyAdjustment += distributionNumerators[i];
+  }
+  return outputBlockCumulativeDistributionMap;
+}
+
+char RandomizedLevel::chooseBlockType() const {
+  if (this->randomEnabled) {
+    int randomChoice = rand() % this->denom;
+
+    return this->blockCumulativeDistributionMap.find(randomChoice)->second;
+  } else {
+    return Level::chooseBlockType();
+  }
+}
+
+void RandomizedLevel::enableRandom() { this->randomEnabled = true; }
+
+void RandomizedLevel::disableRandom() { this->randomEnabled = false; }
+
+void RandomizedLevel::disableRandom(std::string & blockSequenceFile) {
+  this->disableRandom();
+  this->setBlockSequenceFile(blockSequenceFile);
 }
--- Biquadris/Board/Level/level.h
+++ Biquadris/Board/Level/level.h
@@ -2,27 +2,53 @@
 #define LEVEL_H
 
 #include <string>
+#include <fstream>
 #include <vector>
+#include <map>
 #include "../board-proxy.h"
 
 class Level {
-  const int numLevel;
-  const bool heavy;
-  BoardProxy & boardProxy;
-  bool random;
-  std::string sequenceFileName;
-  std::vector<std::unique_ptr<Block>> blockBacklog;
-
+  private:
+    const int numLevel;
+    const bool heavy;
+    std::string blockSequenceFileName;
+    std::ifstream blockSequenceFileStream;
+    std::vector<std::unique_ptr<Block>> blockBacklog;
+  
+    std::unique_ptr<Block> generateBlock(char blockType) const;
   protected:
-    Level(int numLevel, bool heavy, BoardProxy & boardProxy, bool random, std::string sequenceFileName);
-    virtual std::unique_ptr<Block> generateBlock() = 0;
+    BoardProxy & boardProxy;
+
+    Level(int numLevel, bool heavy, BoardProxy & boardProxy, std::string & blockSequenceFileName);
+    void setBlockSequenceFile(std::string & blockSequenceFile);
+    virtual char chooseBlockType() const;
+    virtual ~Level() = default;
 
   public:
     static const int BACKLOG_SIZE = 4;
 
     std::unique_ptr<Block> cycleBlock();
-    virtual bool checkRuleCondition() const { return false; }
-    virtual void executeRuleAction() {}
+    virtual bool checkRuleCondition() const;
+    virtual void executeRuleAction();
+};
+
+class RandomizedLevel : public Level {
+  private:
+    const int denom;
+    const std::map<int, char> blockCumulativeDistributionMap;
+    bool randomEnabled;
+
+    static std::map<int, char> convertNumeratorsToCumulativeDistributionMap(std::vector<int> & distributionNumerators);
+
+  protected:
+    RandomizedLevel(int numLevel, bool heavy, BoardProxy & boardProxy, std::string & blockSequenceFileName, int denomLCM, std::vector<int> & distributionNumerators);
+    char chooseBlockType() const override;
+    virtual ~RandomizedLevel() = default;
+
+  public: 
+    void enableRandom();
+    void disableRandom();
+    void disableRandom(std::string & blockSequenceFile);
 };
 
 #endif
